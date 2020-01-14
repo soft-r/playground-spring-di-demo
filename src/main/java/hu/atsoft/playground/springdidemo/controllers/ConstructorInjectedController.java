2f8cc244e083937020f6098ff15f471712871317
dp <hudomokp@gmail.com>
2020-01-14 19:39:38 +0100

    Use qualifiers to select which service is being injected

--- src/main/java/hu/atsoft/playground/springdidemo/controllers/ConstructorInjectedController.java
+++ src/main/java/hu/atsoft/playground/springdidemo/controllers/ConstructorInjectedController.java
@@ -1,13 +1,14 @@
 package hu.atsoft.playground.springdidemo.controllers;
 
 import hu.atsoft.playground.springdidemo.services.GreetingService;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class ConstructorInjectedController {
     private GreetingService greetingService;
 
-    public ConstructorInjectedController(GreetingService greetingService) {
+    public ConstructorInjectedController(@Qualifier("constructorGreetingService") GreetingService greetingService) {
         this.greetingService = greetingService;
     }
 
--- src/main/java/hu/atsoft/playground/springdidemo/controllers/SetterInjectedController.java
+++ src/main/java/hu/atsoft/playground/springdidemo/controllers/SetterInjectedController.java
@@ -2,6 +2,7 @@ package hu.atsoft.playground.springdidemo.controllers;
 
 import hu.atsoft.playground.springdidemo.services.GreetingService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 
 @Controller
@@ -14,6 +15,7 @@ public class SetterInjectedController {
     }
 
     @Autowired
+    @Qualifier("setterGreetingService")
     public void setGreetingService(GreetingService greetingService) {
         this.greetingService = greetingService;
     }
